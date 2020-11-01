47f6d5acef811d05c31e2fc3a2ef66f4cca47270
Petr Nohavica <petr.nohavica@gmail.com>
2020-11-01 18:32:23 +0100

    Code cleanup

--- beli.py
+++ beli.py
@@ -2,65 +2,89 @@ import click
 import requests
 import json
 
+class BeliController:
+    def __init__(self, ip):
+        self._ip = ip
+        self._host = f"{ip}:5000" 
+        self._headers = {
+            "Host": self._host,
+            "Connection": "Keep-Alive",
+            "Accept-Encoding": "gzip",
+            "User-Agent": "okhttp/3.10.0"
+        }
+
+    def _get(self, cmd, json=None):
+        r = requests.get(url=f"http://{self._host}/{cmd}", headers=self._headers, json=json)
+        if r.status_code != 200:
+            print(f"Device responded with {r.status_code}")
+        return r
+
+    def _post(self, cmd, json=None):
+        r = requests.post(url=f"http://{self._host}/{cmd}", headers=self._headers, json=json)
+        if r.status_code != 200:
+            print(f"Device responded with {r.status_code}")
+        return r
+        
+    def get_detail(self):
+        r = self._get("getDetail")
+        print(r.json())
+    def wifi_list(self):   
+        r = self._post("getWifi")
+        wifi_list = r.json()["data"]["result"]
+        print(json.dumps(wifi_list))
+    def finish(self):
+        print("not implemented")
+    def turn_off(self):
+        self._post("setSta", json={"status": 0})
+    def turn_on(self):
+        self._post("setSta", json={"status": 1})
+    def get_state(self):
+        r = self._post("getSta")
+        return r.json()
+    def toggle(self):
+        state = self.get_state()["data"]["status"]
+        print(state)
+        if state == 1:
+            self.turn_off()
+        else:
+            self.turn_on()
+
 @click.group()
 @click.option("--ip", default="192.168.25.1", help="IP of the device")
 def cli(ip):
-    global ip_addr, host, headers
-    ip_addr = ip
-    host = f"{ip_addr}:5000" 
-    headers = {
-        "Host": host,
-        "Connection": "Keep-Alive",
-        "Accept-Encoding": "gzip",
-        "User-Agent": "okhttp/3.10.0"
-    }
+    global beli
+    beli = BeliController(ip)
 
 @cli.group()
 def setup():
     pass
-
 @setup.command()
 def get_detail():
-    r = requests.get(url=f"http://{host}/getDetail", headers=headers)
-    if r.status_code != 200:
-        print("Device responded with error code", r.status_code)
-        return
-    print(r.json())
-
+    beli.get_detail()
 @setup.command()
 def wifi_list():
-    r = requests.post(url=f"http://{host}/getWifi", headers=headers)
-    if r.status_code != 200:
-        print("Device responded with error code", r.sttus_code)
-        return
-   
-    wifi_list = r.json()["data"]["result"]
-    print(json.dumps(wifi_list))
+    beli.wifi_list()
 @setup.command()
 def finish():
-    print("Not implemented")
+    beli.finish()
 
 @cli.command()
 def turn_on():
-    r = requests.post(url=f"http://{host}/setSta", headers=headers,json={"status": 1})
-    if r.status_code != 200:
-        print("Device responded with error code", r.sttus_code)
-        return
-
+    beli.turn_on()
 @cli.command()
 def turn_off():
-    r = requests.post(url=f"http://{host}/setSta", headers=headers,json={"status": 0})
-    if r.status_code != 200:
-        print("Device responded with error code", r.sttus_code)
-        return
-
+    beli.turn_off()
 @cli.command()
 def get_state():
-    r = requests.post(url=f"http://{host}/getSta", headers=headers)
-    if r.status_code != 200:
-        print("Device responded with error code", r.sttus_code)
-        return
-    print(r.json())
+    beli.get_state()
+@cli.command()
+def toggle():
+    beli.toggle()
+@cli.command()
+def cycle():
+    for i in range(10):
+        beli.turn_off()
+        beli.turn_on()
 
 if __name__ == "__main__":
     cli()
